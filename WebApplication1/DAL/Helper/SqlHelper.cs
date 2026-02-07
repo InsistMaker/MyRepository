10c302967c762e10dbcedfb78cc2f47db89f5764
InsistMaker <9203352735@qq.com>
2026-02-07 17:44:28 +0800

    添加代码

--- WebApplication1/DAL/Helper/SqlHelper.cs
+++ WebApplication1/DAL/Helper/SqlHelper.cs
@@ -3,11 +3,76 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace DAL.Helper
 {
     public class SqlHelper
     {
+        private static readonly string connString = ConfigurationManager.ConnectionStrings["connString"].ToString();
+        public static int Update(string sql)
+        {
+            SqlConnection conn = new SqlConnection(connString);
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            try
+            {
+                conn.Open();
+                return cmd.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
 
+                throw;
+            }finally
+            {
+                conn.Close();
+            }
+        }
+        /// <summary>
+        /// 执行单一结果查询
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <returns></returns>
+        public static object GetSingleResult(string sql)
+        {
+            SqlConnection conn = new SqlConnection(connString);
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            try
+            {
+                conn.Open();
+                return cmd.ExecuteScalar();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+        /// <summary>
+        /// 执行结果集的查询
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <returns></returns>
+        public static SqlDataReader GetReader(string sql)
+        {
+            SqlConnection conn = new SqlConnection(connString);
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            try
+            {
+                conn.Open();
+                return cmd.ExecuteReader(CommandBehavior.CloseConnection);
+            }
+            catch (Exception)
+            {
+                conn.Close();
+                throw;
+            }
+        }
     }
 }
