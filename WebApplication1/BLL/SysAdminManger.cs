5a3cebacc3f6596e6394c2ba29aaa024297a36b9
InsistMaker <9203352735@qq.com>
2026-02-08 12:15:16 +0800

    提交代码

--- WebApplication1/BLL/SysAdminManger.cs
+++ WebApplication1/BLL/SysAdminManger.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Models;
+using DAL;
+using System.Web;
+//using System.Net.Http;
 
 namespace BLL
 {
-    class SysAdminManger
+    public class SysAdminManger
     {
+        public SysAdmin AdminLogin(SysAdmin objAdmin)
+        {
+            objAdmin = new SysAdminService().AdminLogin(objAdmin);
+            if (objAdmin !=null)
+            {
+                HttpContext.Current.Session["CurrentName"] = objAdmin;
+            }
+            return objAdmin;
+        }
     }
 }
--- WebApplication1/DAL/SysAdminService.cs
+++ WebApplication1/DAL/SysAdminService.cs
@@ -1,12 +1,40 @@
-﻿using System;
+﻿using Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace DAL
 {
-    class SysAdminService
+    public class SysAdminService
     {
+        public SysAdmin AdminLogin(SysAdmin objAdmin)
+        {
+            string sql = "select AdminName from admins where loginID={0} and loginPwd='{1}'";
+            sql = string.Format(sql, objAdmin.LoginId, objAdmin.LoginPwd);
+            
+            try
+            {
+                SqlDataReader objReader = Helper.SqlHelper.GetReader(sql);
+                if (objReader.Read())
+                {
+                    objAdmin.AdminName= objReader["AdminName"].ToString();
+                    objReader.Close();
+                }
+                else
+                {
+                    objAdmin = null;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return objAdmin;
+        }
     }
 }
