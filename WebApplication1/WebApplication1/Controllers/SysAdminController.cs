24cdbc63608d05ec541747fb6c86470e2eee2bb0
InsistMaker <920352735@qq.com>
2026-02-11 17:47:58 +0800

    提交代码

--- WebApplication1/WebApplication1/Controllers/SysAdminController.cs
+++ WebApplication1/WebApplication1/Controllers/SysAdminController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Models;
+using BLL;
 
 namespace WebApplication1.Controllers
 {
@@ -12,6 +14,17 @@ namespace WebApplication1.Controllers
         public ActionResult Index()
         {
             return View("AdminLogin");
+        }
+        public ActionResult AdminLogin()
+        {
+            SysAdmin objAdmin = new SysAdmin()
+            {
+                LoginId = Convert.ToInt32(Request.Params["loginId"]),
+                LoginPwd=Request.Params["loginpwd"]
+            };
+            objAdmin = new SysAdminManger().AdminLogin(objAdmin);
+            ViewData["info"]="欢迎"+objAdmin.AdminName;
+            return View();
         }
     }
 }
